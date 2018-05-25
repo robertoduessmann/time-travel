8b62475ac39cd6b262f939f7a7b3b7f1d47dce54
Roberto Duessmann <roberto.duessmann@gmail.com>
2018-05-25 13:31:15 -0300

    Create time-travel Spring Boot project

---
+++ src/test/java/com/space/timetravel/TimeTravelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.space.timetravel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeTravelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
